4eb331f789bf0231c371a692853bacc1739dc5ed
Adel-Alhassani <adelalhuthli@gmail.com>
2024-12-07 13:18:27 +0300

    Fix: texts not appear in release mode

--- lib/service/setting_services.dart
+++ lib/service/setting_services.dart
@@ -1,5 +1,6 @@
 import 'package:convertify/controller/file_controller.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -8,6 +9,7 @@ class SettingServices extends GetxService {
   late SharedPreferences sharedPreferences;
 
   Future<SettingServices> init() async {
+    await ScreenUtil.ensureScreenSize();
     dotenv.load(fileName: ".env");
     sharedPreferences = await SharedPreferences.getInstance();
     MobileAds.instance.initialize();
